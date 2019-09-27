d340a0a13104fe977c3028f72e87e62b3ed5807b
Kingsleyokoro <50120436+Kingsleyokoro@users.noreply.github.com>
2019-09-27 13:30:43 -0600

    Add project files.

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Workshop_5
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Workshop_5.MvcApplication" Language="C#" %>
